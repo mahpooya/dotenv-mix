1ba6286b59785ecb2eeae9d0c88c0df953e46e0e
James Fenwick <james.fenwick@iamproperty.com>
2020-07-03 15:27:04 +0100

    Create Env Mix component

---
+++ index.js
@@ -0,0 +1,36 @@
+const Dotenv = require('dotenv-webpack')
+
+class Env {
+  constructor () {
+    this.options = {}
+  }
+
+  /**
+   * Register the component.
+   *
+   * Ex: env(path, options) {}
+   * Ex: mix.env('.env.production', { systemvars: true });
+   *
+   * @param {string} [path]
+   * @param {options} [options]
+   */
+  register (path, options) {
+    this.options = {
+      ...options,
+      path
+    }
+  }
+
+  /**
+   * Override the generated webpack configuration.
+   *
+   * @param {Object} config
+   */
+  webpackConfig (config) {
+    return {
+      plugins: [new Dotenv(this.options)]
+    }
+  }
+}
+
+module.exports = new Env()
